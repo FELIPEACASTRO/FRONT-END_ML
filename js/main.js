3a28c0eb34c6792288b559ac72686af0080ebd6f
FELIPEACASTRO <felipesp1983@hotmail.com>
2016-11-03 22:05:23 -0200

    FRONT-END_ML
    add page WEB

---
+++ js/main.js
@@ -0,0 +1,7 @@
+;(function(){
+  var product_form = $("#product-form").form();
+  var carousel = $(".similar-products-carousel").carousel({
+    'limitPerPage': 3
+  });
+  var tabs = $("#product-tabs").tabs();
+})();
